af236612e9b64a672383d6928de0d29ccc247faa
Claps <claps@claps.com.cn>
2020-05-23 16:57:13 +0800

    None

--- cite_graph.py
+++ cite_graph.py
@@ -1,7 +1,5 @@
-import time
-from dgl.data import citation_graph as citegrh
 import numpy as np
-import networkx as nx
+from dgl.data import citation_graph as citegrh
 import dgl
 import dgl.function as fn
 
@@ -41,7 +39,6 @@ class CoNet(th.nn.Module):
         x3 = x3.squeeze(1)
 
         weights = self.w / th.sum(self.w, 0)
-        # weights = F.softmax(F.leaky_relu(self.w), 0)
 
         return weights[0] * x1 + weights[1] * x2 + weights[2] * x3
 
@@ -53,9 +50,6 @@ class Net(nn.Module):
         self.layer1 = CoNet(in_channels, hidden_channels)
         self.layer2 = th.nn.BatchNorm1d(hidden_channels)
         self.layer3 = CoNet(hidden_channels, out_channels, f_drop=0)
-        # self.layer1 = GATConv(in_channels, hidden_channels, 1)
-        # self.layer2 = th.nn.BatchNorm1d(hidden_channels)
-        # self.layer3 = GATConv(hidden_channels, out_channels, 1)
 
     def reset_parameters(self):
 
@@ -73,11 +67,9 @@ class Net(nn.Module):
         x = F.dropout(x, p=0.5, training=self.training)
         x = self.layer3(g, x)
         # x = x.squeeze(1)
+        x = F.log_softmax(x, 1)
 
         return x
-
-###############################################################################
-# We load the cora dataset using DGL's built-in data module.
 
 
 def load_data():
@@ -89,36 +81,36 @@ def load_data():
     g = DGLGraph(data.graph)
     return g, features, labels, train_mask, test_mask
 
-###############################################################################
-# When a model is trained, we can use the following method to evaluate
-# the performance of the model on the test dataset:
-
 
 def evaluate(model, g, features, labels, mask):
     model.eval()
     with th.no_grad():
-        logits = model(g, features)
-        logits = logits[mask]
+        res = model(g, features)
+        res = res[mask]
         labels = labels[mask]
-        _, indices = th.max(logits, dim=1)
+        _, indices = th.max(res, dim=1)
         correct = th.sum(indices == labels)
         return correct.item() * 1.0 / len(labels)
 
-###############################################################################
-# We then train the network as follows:
-
 
 g, features, labels, train_mask, test_mask = load_data()
-net = Net(features.size(1), 128, 7)
+
+device = f'cuda:{0}' if th.cuda.is_available() else 'cpu'
+device = th.device(device)
+
+features = features.to(device)
+labels = labels.to(device)
+train_mask = train_mask.to(device)
+test_mask = test_mask.to(device)
+
+net = Net(features.size(1), 128, 7).to(device)
 print(net)
-optimizer = th.optim.Adam(net.parameters(), lr=1e-2)
-dur = []
-for epoch in range(200):
+optimizer = th.optim.Adam(net.parameters(), lr=1e-2, weight_decay=5e-4)
+for epoch in range(500):
 
     net.train()
-    logits = net(g, features)
-    logp = F.log_softmax(logits, 1)
-    loss = F.nll_loss(logp[train_mask], labels[train_mask])
+    res = net(g, features)
+    loss = F.nll_loss(res[train_mask], labels[train_mask])
 
     optimizer.zero_grad()
     loss.backward()
